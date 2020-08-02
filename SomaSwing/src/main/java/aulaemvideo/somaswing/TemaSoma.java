3f29d5351c750e81f90506dd04fe604c908afc3b
ricardopitanga <ricardopitanga@ricardopitanga-ThinkPad-Edge-15>
2020-08-02 02:46:12 -0300

    Teste de sexto commit

--- SomaSwing/src/main/java/aulaemvideo/somaswing/TemaSoma.java
+++ SomaSwing/src/main/java/aulaemvideo/somaswing/TemaSoma.java
@@ -67,8 +67,8 @@ public class TemaSoma extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addComponent(btnSoma, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(4, 4, 4)
-                .addComponent(lblSoma, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(128, Short.MAX_VALUE))
+                .addComponent(lblSoma, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(131, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
